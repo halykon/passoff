ab49db87cb09430a1c96150ee4dea6a48f2fe403
Johann Berger <j.berger@alpin11.com>
2022-04-13 21:29:35 +0200

    + focus search

--- storybook/stories/Search/Search.tsx
+++ storybook/stories/Search/Search.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import React, { useRef } from 'react'
+import { Box, Input, InputGroup, InputLeftElement, InputRightElement, Kbd } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
+import { useHotkeys } from 'react-hotkeys-hook'
 
 interface ISearchProps {
 
 }
 
 export const Search: React.FC<ISearchProps> = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  useHotkeys('ctrl+space', () => {
+    inputRef.current?.focus()
+  })
+
   return (
     <InputGroup>
       <InputLeftElement pointerEvents="none" h="100%">
         <SearchIcon/>
       </InputLeftElement>
-      <Input size="lg" variant="filled" placeholder="Search Passwords" borderRadius={0}/>
+      <Input ref={inputRef} size="lg" variant="filled" placeholder="Search Passwords" borderRadius={0}/>
+      <InputRightElement pointerEvents="none" h="100%" w="100%" justifyContent="flex-end" pr="30px">
+        <Box>press <Kbd>ctrl</Kbd> + <Kbd>space</Kbd> to search</Box>
+      </InputRightElement>
     </InputGroup>
   )
 }
