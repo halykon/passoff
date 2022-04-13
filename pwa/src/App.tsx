b1e39d4095fcaad6c84995a3a9936a1e6edb4c04
Johann Berger <j.berger@alpin11.com>
2022-04-13 20:40:09 +0200

    + pwa build fix

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Search } from '@sbc/Search/Search'
+import { ListItem } from '@sbc/ListItem/ListItem'
 
 const App = () => {
   return (
     <div className="App">
       <Search/>
+      <ListItem/>
     </div>
   )
 }
