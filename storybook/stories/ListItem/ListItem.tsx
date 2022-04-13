5f5b5e4ac9a1f8cb509e18cd8b70d6ca7ec09c81
Johann Berger <j.berger@alpin11.com>
2022-04-13 20:40:09 +0200

    + list item focus

--- storybook/stories/ListItem/ListItem.tsx
+++ storybook/stories/ListItem/ListItem.tsx
@@ -11,12 +11,12 @@ export const ListItem: React.FC<IListItemProps> = () => {
       variant="box"
       display="flex"
       p="15px"
-      _focus={{
-        bg: 'blackAlpha.500',
-        shadow: 'outline',
-      }}
       _hover={{
         bg: 'blackAlpha.300',
+      }}
+      _focus={{
+        bg: 'var(--chakra-colors-blackAlpha-500) !important', // little hack to hover style
+        shadow: 'outline',
       }}
     >
       <Stack direction="row" spacing="15px">
