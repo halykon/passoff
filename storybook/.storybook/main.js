ab362a651c6d780abeadda591b99f4def64cd6b7
Johann Berger <j.berger@alpin11.com>
2022-04-13 20:40:09 +0200

    + upgrade storybook webpack

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,21 +1,16 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@chakra-ui/storybook-addon"
   ],
-  "framework": "@storybook/react",
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto",
-    })
-    return config
-  }
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
 }
