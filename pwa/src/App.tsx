20eefdfe2bd844c1dd51a55ba0b89649cd12fd19
Johann Berger <j.berger@alpin11.com>
2022-04-13 21:13:01 +0200

    + grid layout

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Search } from '@sbc/Search/Search'
 import { ListItem } from '@sbc/ListItem/ListItem'
+import { Flex, Grid, GridItem } from '@chakra-ui/react'
 
 const App = () => {
   return (
-    <div className="App">
+    <Flex h="100vh" direction="column">
       <Search/>
-      <ListItem/>
-    </div>
+      <Grid
+        h="100%"
+        templateColumns="repeat(2, 1fr)"
+      >
+        <GridItem>
+          <ListItem/>
+        </GridItem>
+        <GridItem padding={5} bg="gray.900">
+          pw contents
+        </GridItem>
+      </Grid>
+    </Flex>
   )
 }
 
