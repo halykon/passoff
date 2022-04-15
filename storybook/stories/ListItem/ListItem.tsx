06fe4d8f0f936fb1a4818b335e630229346fc922
Johann Berger <j.berger@alpin11.com>
2022-04-15 12:28:25 +0200

    + divider

--- storybook/stories/ListItem/ListItem.tsx
+++ storybook/stories/ListItem/ListItem.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Heading, Stack, Text, keyframes } from '@chakra-ui/react'
+import { Avatar, Box, Button, Heading, Stack, Text, keyframes, Divider } from '@chakra-ui/react'
 import React from 'react'
 
 const pulsate = keyframes`
@@ -19,29 +19,32 @@ interface IListItemProps {
 
 export const ListItem: React.FC<IListItemProps> = ({ onSelect, isActive, name, username }) => {
   return (
-    <Box p="2px" transition=".35s" bg={isActive ? 'blackAlpha.500' : ''} _hover={{ bg: 'blackAlpha.300' }} _focusWithin={{ bg: 'var(--chakra-colors-blackAlpha-500) !important' }}>
-      <Button
-        className="selectable"
-        isActive={isActive}
-        onFocus={onSelect}
-        variant="box"
-        display="flex"
-        p="15px"
-        color="primary.500"
-        _focus={{
-          animation: `${pulsate} 1s 1s infinite alternate`,
-          shadow: '0 0 0 2px',
-        }}
-      >
-        <Stack direction="row" spacing="15px" color="white">
-          <Avatar name={name}/>
-          <Box>
-            <Heading size="md">{name}</Heading>
-            <Text size="md">{username}</Text>
-          </Box>
-        </Stack>
-      </Button>
-    </Box>
+    <>
+      <Box p="2px" transition=".35s" bg={isActive ? 'blackAlpha.500' : ''} _hover={{ bg: 'blackAlpha.300' }} _focusWithin={{ bg: 'var(--chakra-colors-blackAlpha-500) !important' }}>
+        <Button
+          className="selectable"
+          isActive={isActive}
+          onFocus={onSelect}
+          variant="box"
+          display="flex"
+          p="15px"
+          color="primary.500"
+          _focus={{
+            animation: `${pulsate} 1s 1s infinite alternate`,
+            shadow: '0 0 0 2px',
+          }}
+        >
+          <Stack direction="row" spacing="15px" color="white">
+            <Avatar name={name}/>
+            <Box>
+              <Heading size="md">{name}</Heading>
+              <Text size="md">{username}</Text>
+            </Box>
+          </Stack>
+        </Button>
+      </Box>
+      <Divider/>
+    </>
   )
 }
 
